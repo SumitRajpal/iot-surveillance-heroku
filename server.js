424eba6fef2110cd2dcf0fd121f0824119b8953c
sumit rajpal <sumit12rds@gmail.com>
2021-01-02 20:13:37 +0530

    first commit

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const express = require('express');
 const WebSocket = require('ws');
 const app = express();
 
-const WS_PORT  =  80;
+const WS_PORT  = 8888;
 const HTTP_PORT =  process.env.PORT || 3000;
 
 const wsServer = new WebSocket.Server({port: WS_PORT}, ()=> console.log(`WS Server is listening at ${WS_PORT}`));
 let connectedClients = [];
-console.log()
 wsServer.on('connection', (ws, req)=>{
-    console.log('Connected');
+    console.log('Connected',ws);
     connectedClients.push(ws);
 
     ws.on('message', data => {
