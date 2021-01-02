31537c9e6c11f774b5d186ed408247f8eff09686
sumit rajpal <sumit12rds@gmail.com>
2021-01-02 22:03:50 +0530

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const WebSocket = require('ws');
+const { networkInterfaces } = require('os');
 const app = express();
 const WS_PORT  = 30895;
 const HTTP_PORT =  process.env.PORT || 3000;
@@ -8,6 +9,20 @@ console.log(process.env.PORT)
 const wsServer = new WebSocket.Server({port: WS_PORT}, ()=> console.log(`WS Server is listening at ${WS_PORT}`));
 let connectedClients = [];
 console.log(wsServer);
+const nets = networkInterfaces();
+const results = Object.create(null); // Or just '{}', an empty object
+
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        if (net.family === 'IPv4' && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
+        }
+    }
+}
 wsServer.on('connection', (ws, req)=>{
     console.log('Connected',req.connection.remoteAddress);
     connectedClients.push(ws);
