a008cc209b4ef374249ca058f8788161b070115f
sumit rajpal <sumit12rds@gmail.com>
2021-01-03 08:52:46 +0530

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const WebSocket = require('ws');
 const { networkInterfaces } = require('os');
 const app = express();
-const WS_PORT  = 30895;
+const WS_PORT  = process.env.PORT || 80;
 const HTTP_PORT =  process.env.PORT || 3000;
 console.log(process.env.PORT)
 const wsServer = new WebSocket.Server({port: WS_PORT}, ()=> console.log(`WS Server is listening at ${WS_PORT}`));
